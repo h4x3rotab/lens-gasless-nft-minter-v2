06a94319019e519fe3cbd7ed54dd7c3f00c9db41
Al Luken <al5031@nyu.edu>
2023-10-23 12:04:53 -0700

    remove router

--- src/components/utils/DummyNavbar.tsx
+++ src/components/utils/DummyNavbar.tsx
@@ -1,18 +1,12 @@
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import demoLogo from "../../../public/assets/demo_logo.svg";
 
 export default function Navbar() {
-  const router = useRouter();
-
   return (
     <div>
       <div className="navbar font-mono">
         <div className="flex-1 ml-4">
-          <div
-            className="cursor-pointer flex items-center"
-            onClick={() => router.push("/")}
-          >
+          <div className="cursor-pointer flex items-center">
             <a
               href="https://accountkit.alchemy.com/introduction.html"
               target="_blank"
